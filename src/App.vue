89f3d1b306422860bd391a48df7bd869f486969b
DESKTOP-BNKDJDK\Ddooby <chjeong1987@gmail.com>
2025-03-20 16:34:28 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="parent">
+    <h1 class="display-1">App</h1>
+    <button class="btn btn-primary">button</button>
+  </div>
+  <h1>heading 1</h1>
+</template>
+
+<!-- lang="scss" 를 쓰면 sass 문법을 사용할 수 있다. -->
+<style scoped lang="scss">
+  // .parent h1 {
+  //   color: red;
+  // }
+
+  // .parent button {
+  //   color: yellow;
+  // }
+
+  // Nesting 문법
+  .parent {
+    h1 { color: red; }
+    button { color: yellow }
+  }
+</style>
